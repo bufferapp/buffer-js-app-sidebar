69a895d6387f1a083d3649dfa9bf16a433a7f71e
Maya Uribe <mariale.uribe@gmail.com>
2019-03-01 12:03:13 +0100

    Reversing validation to track less free users for FS.

--- src/middleware.js
+++ src/middleware.js
@@ -3,17 +3,6 @@ import {
   actionTypes as dataFetchActionTypes,
 } from '@bufferapp/async-data-fetch'
 
-const maxRandomValue = 10 * 7
-const getRandomInt = () => {
-  return Math.floor(Math.random() * Math.floor(maxRandomValue))
-}
-
-const identifyUser = (id, planName) => {
-  window.FS.identify(id, {
-    pricingPlan_str: planName,
-  })
-}
-
 export default ({ dispatch, getState }) => next => action => {
   next(action)
   switch (action.type) {
@@ -30,14 +19,9 @@ export default ({ dispatch, getState }) => next => action => {
         productFeatures: { planName },
       } = getState()
       if (window && window.FS && window.FS.identify) {
-        if (planName === 'free') {
-          // Validation to track 1 out of 10 free users
-          if (getRandomInt() === 1) {
-            identifyUser(id, planName)
-          }
-        } else {
-          identifyUser(id, planName)
-        }
+        window.FS.identify(id, {
+          pricingPlan_str: planName,
+        })
       }
       break
     default:
