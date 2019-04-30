15249ebb04d1d36fbf170f1835b1f2942562f7b3
Maya Uribe <mariale.uribe@gmail.com>
2019-04-30 17:00:42 +0200

    Adding identify script from appCues to initialize users.

--- src/middleware.js
+++ src/middleware.js
@@ -14,14 +14,36 @@ export default ({ dispatch, getState }) => next => action => {
       )
       break
     case `user_${dataFetchActionTypes.FETCH_SUCCESS}`:
-      const { id } = action.result
+      const {
+        id,
+        createdAt,
+        plan,
+        planCode,
+        trial,
+        is_business_user,
+      } = action.result
       const {
         productFeatures: { planName },
       } = getState()
-      if (window && window.FS && window.FS.identify) {
-        window.FS.identify(id, {
-          pricingPlan_str: planName,
-        })
+      if (window) {
+        if (window.FS && window.FS.identify) {
+          window.FS.identify(id, {
+            pricingPlan_str: planName,
+          })
+        }
+        if (window.Appcues) {
+          if (is_business_user) {
+            window.Appcues.identify(id, {
+              name: id, // current user's name
+              createdAt, // Unix timestamp of user signup date
+              plan, // Current user’s plan type
+              planCode, // Current user’s plan tier
+              onTrial: trial.onTrial,
+              trialLength: trial.trialLength,
+              trialTimeRemaining: trial.trialTimeRemaining,
+            })
+          }
+        }
       }
       break
     default:
